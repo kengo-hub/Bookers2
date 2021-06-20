ad86933107571ed0c9e15a468d2382940a42c219
kengo-hub <shoot.bm@gmail.com>
2021-06-20 20:25:34 +0000

    [update]4-10

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+    
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
+  
+  
+  
+  
+end
